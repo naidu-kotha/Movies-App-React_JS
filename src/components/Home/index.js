1cc668698f27af52e77758ebf8536900dc7dd324
naidu-kotha <kpssnaidu@gmail.com>
2022-12-25 17:15:23 +0000

    added api calls

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,17 +12,20 @@ const apiStatusConstants = {
 
 class Home extends Component {
   state = {
-    apiStatus: apiStatusConstants.initial,
+    originalMoviesApiStatus: apiStatusConstants.initial,
+    trendingMoviesApiStatus: apiStatusConstants.initial,
     randomOriginalsMovie: '',
-    originalsMovieData: [],
+    originalMoviesArray: [],
+    trendingMoviesArray: [],
   }
 
   componentDidMount() {
     this.getOriginalsApi()
+    this.getTrendingMoviesApi()
   }
 
   getOriginalsApi = async () => {
-    this.setState({apiStatus: apiStatusConstants.inProgress})
+    this.setState({originalMoviesApiStatus: apiStatusConstants.inProgress})
     const jwtToken = Cookies.get('jwt_token')
     const originalsApiUrl = 'https://apis.ccbp.in/movies-app/originals'
     const options = {
@@ -32,14 +35,14 @@ class Home extends Component {
       },
     }
     const response = await fetch(originalsApiUrl, options)
-    console.log(response)
+    // console.log(response)
     if (response.ok) {
       const originalsData = await response.json()
-      console.log(originalsData)
+      //   console.log(originalsData)
       const randomMovieDataIndex = Math.floor(
         Math.random() * originalsData.results.length,
       )
-      console.log(randomMovieDataIndex)
+      //   console.log(randomMovieDataIndex)
 
       const updatedOriginalsData = originalsData.results.map(eachResult => ({
         id: eachResult.id,
@@ -50,24 +53,126 @@ class Home extends Component {
       }))
 
       const randomOriginalsMovie = updatedOriginalsData[randomMovieDataIndex]
-      console.log(randomOriginalsMovie)
+      //   console.log(randomOriginalsMovie)
       this.setState({
         randomOriginalsMovie,
-        originalsMovieData: updatedOriginalsData,
+        originalMoviesArray: updatedOriginalsData,
+        originalMoviesApiStatus: apiStatusConstants.success,
       })
+    } else {
+      this.setState({originalMoviesApiStatus: apiStatusConstants.failure})
     }
   }
 
-  render() {
+  getTrendingMoviesApi = async () => {
+    this.setState({trendingMoviesApiStatus: apiStatusConstants.inProgress})
+    const trendingMoviesUrl = 'https://apis.ccbp.in/movies-app/trending-movies'
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(trendingMoviesUrl, options)
+    // console.log(response)
+    if (response.ok) {
+      const data = await response.json()
+      //   console.log(data)
+      const trendingMoviesArray = data.results.map(eachResult => ({
+        id: eachResult.id,
+        backdropPath: eachResult.backdrop_path,
+        overview: eachResult.overview,
+        posterPath: eachResult.poster_path,
+        title: eachResult.title,
+      }))
+      this.setState({
+        trendingMoviesArray,
+        trendingMoviesApiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({trendingMoviesApiStatus: apiStatusConstants.failure})
+    }
+  }
+
+  renderFailurePoster = () => (
+    <div className="failure-poster-container">
+      <h1>Something Went Wrong</h1>
+    </div>
+  )
+
+  renderPoster = () => {
+    const {originalMoviesApiStatus} = this.state
+
+    switch (originalMoviesApiStatus) {
+      case apiStatusConstants.success:
+        return this.renderTopCard()
+      //   case apiStatusConstants.failure:
+      //     return this.renderFailurePoster()
+      //   case apiStatusConstants.inProgress:
+      //     return this.renderLoadingView()
+      default:
+        return null
+    }
+  }
+
+  //   renderOriginalMovies = () => {
+  //       const {originalMoviesApiStatus} = this.state
+
+  //     switch (originalMoviesApiStatus) {
+  //       case apiStatusConstants.success:
+  //         return this.
+  //       case apiStatusConstants.failure:
+  //         return this.renderFailureView()
+  //       case apiStatusConstants.inProgress:
+  //         return this.renderLoadingView()
+  //       default:
+  //         return null
+  //     }
+  //   }
+
+  //   renderTrendingMovies = () => {
+  //       const {trendingMoviesApiStatus} = this.state
+
+  //     switch (originalMoviesApiStatus) {
+  //       case apiStatusConstants.success:
+  //         return this.
+  //       case apiStatusConstants.failure:
+  //         return this.renderFailureView()
+  //       case apiStatusConstants.inProgress:
+  //         return this.renderLoadingView()
+  //       default:
+  //         return null
+  //     }
+  //   }
+
+  renderTopCard = () => {
     const {randomOriginalsMovie} = this.state
     const {overview, backdropPath, title} = randomOriginalsMovie
     return (
       <div
-        className="home-bg"
-        style={{backgroundImage: `{url(${backdropPath})}`}}
+        className="home-top-bg"
+        style={{backgroundImage: `url(${backdropPath})`}}
       >
         <Header />
+        <div className="poster-details-container">
+          <h1 className="poster-title">{title}</h1>
+          <p className="poster-overview">{overview}</p>
+          <button type="button" className="poster-btn">
+            Play
+          </button>
+        </div>
       </div>
+    )
+  }
+
+  render() {
+    return (
+      <>
+        {this.renderPoster()}
+        {/* {this.renderTrendingMovies()} */}
+        {/* {this.renderOriginalMovies()} */}
+      </>
     )
   }
 }
