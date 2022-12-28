8c00e8d960c0c5d7ef91b3aba83f28bb1c2314c0
naidu-kotha <kpssnaidu@gmail.com>
2022-12-28 11:37:23 +0000

    popular route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom'
 import Login from './components/Login'
 import ProtectedRoute from './components/ProtectedRoute'
 import Home from './components/Home'
+import PopularMovies from './components/PopularMovies'
 
 import './App.css'
 
@@ -10,6 +11,7 @@ const App = () => (
   <Switch>
     <Route exact path="/login" component={Login} />
     <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/popular" component={PopularMovies} />
   </Switch>
 )
 export default App
---
+++ src/components/PopularMovies/index.js
@@ -0,0 +1,124 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
+
+import Header from '../Header'
+import './index.css'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+class PopularMovies extends Component {
+  state = {apiStatus: apiStatusConstants.initial, popularMoviesArray: []}
+
+  componentDidMount() {
+    this.getPopularMoviesApi()
+  }
+
+  getPopularMoviesApi = async () => {
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+    const url = 'https://apis.ccbp.in/movies-app/popular-movies'
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'Get',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(url, options)
+    // console.log(response)
+    if (response.ok) {
+      const data = await response.json()
+      console.log(data)
+      const updatedData = data.results.map(eachResult => ({
+        id: eachResult.id,
+        backdropPath: eachResult.backdrop_path,
+        posterPath: eachResult.poster_path,
+        title: eachResult.title,
+      }))
+      console.log(updatedData)
+      this.setState({
+        apiStatus: apiStatusConstants.success,
+        popularMoviesArray: updatedData,
+      })
+    } else {
+      this.setState({apiStatus: apiStatusConstants.failure})
+    }
+  }
+
+  renderLoadingView = () => (
+    <div className="popular-movies-loader" testid="loader">
+      <Loader type="TailSpin" color="#D81F26" height={80} width={80} />
+    </div>
+  )
+
+  reloadPage = () => {
+    this.getPopularMoviesApi()
+  }
+
+  renderFailureView = () => (
+    <div className="popular-movies-failure-container">
+      <img
+        src="https://res.cloudinary.com/dck3ikgrn/image/upload/v1672225767/Failure_view_fpucib.png"
+        alt="failure view"
+        className="popular-movies-failure-image"
+      />
+      <p className="popular-movies-failure-msg">
+        Something went wrong. Please try again
+      </p>
+      <button
+        type="button"
+        className="popular-movies-retry-btn"
+        onClick={this.reloadPage}
+      >
+        Try Again
+      </button>
+    </div>
+  )
+
+  renderSuccessView = () => {
+    const {popularMoviesArray} = this.state
+    return (
+      <ul className="popular-movies-container">
+        {popularMoviesArray.map(eachMovie => {
+          const {title, posterPath} = eachMovie
+          return (
+            <li key={title}>
+              <img src={posterPath} alt={title} className="popular-movie-img" />
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+
+  renderPopularMovies = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      case apiStatusConstants.success:
+        return this.renderSuccessView()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return (
+      <div className="popular-movies-bg">
+        <Header />
+        {this.renderPopularMovies()}
+      </div>
+    )
+  }
+}
+
+export default PopularMovies
