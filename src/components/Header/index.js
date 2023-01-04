feb61750d07d3b8a642b6f7e7bd63098cab1a424
naidu-kotha <kpssnaidu@gmail.com>
2023-01-04 08:45:10 +0000

    search-container

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,45 +1,69 @@
+import {Component} from 'react'
 import {Link} from 'react-router-dom'
+import {HiOutlineSearch} from 'react-icons/hi'
 
 import './index.css'
 
-const Header = () => (
-  <nav className="nav-bar">
-    <div className="nav-items-container">
-      <Link to="/" className="header-link">
-        <img
-          src="https://res.cloudinary.com/dck3ikgrn/image/upload/v1671795411/Movies%20App/Movies_Logo_iassdx.png"
-          alt="website logo"
-          className="header-logo"
-        />
-      </Link>
-      <ul className="menu-container">
-        <li className="menu-item">
+class Header extends Component {
+  state = {searchClicked: false}
+
+  toggleSearchContainer = () => {
+    const {searchClicked} = this.state
+    this.setState({searchClicked: !searchClicked})
+  }
+
+  render() {
+    const {searchClicked} = this.state
+    const searchContainerClassName = searchClicked
+      ? 'search-container'
+      : 'hide-elements'
+    const searchIconClassName = searchClicked ? 'hide-elements' : 'search-logo'
+
+    return (
+      <nav className="nav-bar">
+        <div className="nav-items-container">
           <Link to="/" className="header-link">
-            Home
+            <img
+              src="https://res.cloudinary.com/dck3ikgrn/image/upload/v1671795411/Movies%20App/Movies_Logo_iassdx.png"
+              alt="website logo"
+              className="header-logo"
+            />
           </Link>
-        </li>
-        <li className="menu-item">
-          <Link to="/popular" className="header-link">
-            Popular
-          </Link>
-        </li>
-      </ul>
-    </div>
-    <div className="nav-items-container">
-      <button type="button" className="search-btn" testid="searchButton">
-        <img
-          src="https://res.cloudinary.com/dck3ikgrn/image/upload/v1671882999/Movies%20App/searchsearch_icon_lmnkos.svg"
-          alt="search logo"
-          className="search-logo"
-        />
-      </button>
-      <img
-        src="https://res.cloudinary.com/dck3ikgrn/image/upload/v1671881458/Movies%20App/Avatarprofile_lyxntc.jpg"
-        alt="profile"
-        className="profile-img"
-      />
-    </div>
-  </nav>
-)
+          <ul className="menu-container">
+            <li className="menu-item">
+              <Link to="/" className="header-link">
+                Home
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/popular" className="header-link">
+                Popular
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="nav-items-container">
+          <div className={searchContainerClassName}>
+            <input type="search" className="search-bar" />
+            <HiOutlineSearch className="search-logo" />
+          </div>
+          <button
+            type="button"
+            className="search-btn"
+            testid="searchButton"
+            onClick={this.toggleSearchContainer}
+          >
+            <HiOutlineSearch className={searchIconClassName} />
+          </button>
+          <img
+            src="https://res.cloudinary.com/dck3ikgrn/image/upload/v1671881458/Movies%20App/Avatarprofile_lyxntc.jpg"
+            alt="profile"
+            className="profile-img"
+          />
+        </div>
+      </nav>
+    )
+  }
+}
 
 export default Header
